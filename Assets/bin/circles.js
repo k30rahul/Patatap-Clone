36f592ebb8183e6401eeacf71de7a1a1ca3c7b12
Rahul Kaliyath <31266923+k30rahul@users.noreply.github.com>
2019-07-15 00:03:49 +0530

    circles.js

---
+++ Assets/bin/circles.js
@@ -0,0 +1,159 @@
+var keyData={
+	a:{
+	color:"white",
+	sound:new Howl({
+	src:['Assets/sound/bubbles.mp3']
+})
+},
+s:{
+	color:"#5147f5",
+	sound:new Howl({
+	src:['Assets/sound/clay.mp3']
+})
+},
+d:{
+	color:"#5d6a95",
+	sound:new Howl({
+	src:['Assets/sound/confetti.mp3']
+})
+},
+f:{
+	color:"#b178f5",
+	sound:new Howl({
+	src:['Assets/sound/corona.mp3']
+})
+},
+g:{
+	color:"#b3f13d",
+	sound:new Howl({
+	src:['Assets/sound/dotted-spiral.mp3']
+})
+},
+h:{
+	color:"#4f78f8",
+	sound:new Howl({
+	src:['Assets/sound/flash-1.mp3']
+})
+},
+j:{
+	color:"#a7010b",
+	sound:new Howl({
+	src:['Assets/sound/flash-2.mp3']
+})
+},
+k:{
+	color:"#d9be4e",
+	sound:new Howl({
+	src:['Assets/sound/flash-3.mp3']
+})
+},
+l:{
+	color:"#7477e3",
+	sound:new Howl({
+	src:['Assets/sound/glimmer.mp3']
+})
+},
+c:{
+	color:"#c5be5b",
+	sound:new Howl({
+	src:['Assets/sound/moon.mp3']
+})
+},
+z:{
+	color:"#84dded",
+	sound:new Howl({
+	src:['Assets/sound/pinwheel.mp3']
+})
+},
+x:{
+	color:"#d9d4e2",
+	sound:new Howl({
+	src:['Assets/sound/piston-1.mp3']
+})
+},
+v:{
+	color:"#db1d27",
+	sound:new Howl({
+	src:['Assets/sound/piston-1.mp3']
+})
+},
+b:{
+	color:"#26fd3c",
+	sound:new Howl({
+	src:['Assets/sound/piston-2.mp3']
+})
+},
+n:{
+	color:"#c589c3",
+	sound:new Howl({
+	src:['Assets/sound/piston-3.mp3']
+})
+},
+m:{
+	color:"#71c3bd",
+	sound:new Howl({
+	src:['Assets/sound/prism-1.mp3']
+})
+},
+q:{
+	color:"#56816c",
+	sound:new Howl({
+	src:['Assets/sound/prism-2.mp3']
+})
+},
+w:{
+	color:"#d35780",
+	sound:new Howl({
+	src:['Assets/sound/prism-3.mp3']
+})
+},
+e:{
+	color:"#e0da13",
+	sound:new Howl({
+	src:['Assets/sound/splits.mp3']
+})
+},
+r:{
+	color:"#3c9fa3",
+	sound:new Howl({
+	src:['Assets/sound/squiggle.mp3']
+})
+},
+t:{
+	color:"#10cf04",
+	sound:new Howl({
+	src:['Assets/sound/strike.mp3']
+})
+},
+y:{
+	color:"#cdac2f",
+	sound:new Howl({
+	src:['Assets/sound/suspension.mp3']
+})
+},
+u:{
+	color:"#fed6c2",
+	sound:new Howl({
+	src:['Assets/sound/timer.mp3']
+})
+},
+i:{
+	color:"#c5be5b",
+	sound:new Howl({
+	src:['Assets/sound/ufo.mp3']
+})
+},
+o:{
+	color:"#b70948",
+	sound:new Howl({
+	src:['Assets/sound/veil.mp3']
+})
+},
+p:{
+	color:"#b404b3",
+	sound:new Howl({
+	src:['Assets/sound/wipe.mp3']
+})
+}
+
+}; 
